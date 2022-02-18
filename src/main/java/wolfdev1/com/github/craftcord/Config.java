3aed79b978c4141f1117d27666aafbd1701ad287
wolfdev1 <91499295+wolfdev1@users.noreply.github.com>
2022-02-18 19:12:47 +0100

    Refactor

--- src/main/java/wolfdev1/com/github/craftcord/Config.java
+++ src/main/java/wolfdev1/com/github/craftcord/Config.java
@@ -1,7 +1,7 @@
 package wolfdev1.com.github.craftcord;
 
 public class Config {
-    public static String BOT_TOKEN = "Your token here";
+    public static String BOT_TOKEN = "Your bot token here";
     public static String CHANNEL_ID = "940697475088547870";
     public static String GAME_OF_ACTIVITY = "Minecraft 1.18.1";
 }
