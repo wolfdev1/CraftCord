b2069dcb2e9800fb6d10b8e376b1f069ccb15ce2
wolfdev1 <91499295+wolfdev1@users.noreply.github.com>
2022-02-18 19:49:10 +0100

    Added try catch method for error when player join response time is very long

--- src/main/java/wolfdev1/com/github/craftcord/Events/PlayerJoin.java
+++ src/main/java/wolfdev1/com/github/craftcord/Events/PlayerJoin.java
@@ -1,27 +1,34 @@
 package wolfdev1.com.github.craftcord.Events;
 
 import net.dv8tion.jda.api.EmbedBuilder;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.awt.*;
 import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+
 import static wolfdev1.com.github.craftcord.CraftCord.jda;
 
 public class PlayerJoin implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        EmbedBuilder joinEb = new EmbedBuilder()
-                .setAuthor(event.getPlayer().getName(), null, "https://crafatar.com/avatars/" + event.getPlayer().getUniqueId())
-                .setTitle(event.getPlayer().getName() + " has been joined to the server")
-                .setDescription("Say hello to " + event.getPlayer().getName() + " :wave:")
-                .setTimestamp(Instant.now())
-                .setFooter("Minecraft 1.18.1")
-                .setColor(Color.decode("#94ffbf"))
-                ;
+        try{
+            EmbedBuilder joinEb = new EmbedBuilder()
+                    .setAuthor(event.getPlayer().getName(), null, "https://crafatar.com/avatars/" + event.getPlayer().getUniqueId())
+                    .setTitle(event.getPlayer().getName() + " has been joined to the server")
+                    .setDescription("Say hello to " + event.getPlayer().getName() + " :wave:")
+                    .setTimestamp(Instant.now())
+                    .setFooter("Minecraft 1.18.1")
+                    .setColor(Color.decode("#94ffbf"))
+                    ;
 
-        jda.getTextChannelById("940697475088547870").sendMessageEmbeds(joinEb.build()).queue();
+            jda.getTextChannelById("940697475088547870").sendMessageEmbeds(joinEb.build()).queueAfter(3, TimeUnit.SECONDS);
+        }catch (IllegalStateException e) {
+            Bukkit.getLogger().info("The plugin has tried to get the information of a player who joined the server without success, CraftCord error #372");
+        }
     }
 }
