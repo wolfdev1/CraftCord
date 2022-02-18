6859f9d676af98c9926058334f8501d01d1fcff5
wolfdev1 <91499295+wolfdev1@users.noreply.github.com>
2022-02-18 19:32:38 +0100

    Added if the sender of message is bot ignore this

--- src/main/java/wolfdev1/com/github/craftcord/Events/MemberMessage.java
+++ src/main/java/wolfdev1/com/github/craftcord/Events/MemberMessage.java
@@ -7,14 +7,17 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.jetbrains.annotations.NotNull;
 
+
 public class MemberMessage extends ListenerAdapter implements Listener {
+
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+
         if(!event.getChannel().getId().equals("940697475088547870"))
         {
             return;
         }else{
-            if(event.getMessage().getId().equals(event.getJDA().getSelfUser().getId())) {
+            if(event.getMember().getId().equals(event.getJDA().getSelfUser().getId())) {
                 return;
             }else{
                 Bukkit.broadcastMessage
