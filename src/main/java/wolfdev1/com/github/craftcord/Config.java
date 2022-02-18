c44d23b96ead8f0f77368f9121e6626ff87e3080
wolfdev1 <91499295+wolfdev1@users.noreply.github.com>
2022-02-19 00:03:19 +0100

    Changed BOT_TOKEN value from real bot token to "Your token here"

--- src/main/java/wolfdev1/com/github/craftcord/Config.java
+++ src/main/java/wolfdev1/com/github/craftcord/Config.java
@@ -1,7 +1,7 @@
 package wolfdev1.com.github.craftcord;
 
 public class Config {
-    public static String BOT_TOKEN = "OTM4NDM3MzU2NTM1NTcwNDYy.YfqR5Q.41-WFerVRwtwTEDPV0gowh90wJ8";
+    public static String BOT_TOKEN = "Your token here";
     public static String CHANNEL_ID = "940697475088547870";
     public static String GAME_OF_ACTIVITY = "Minecraft 1.18.1";
 }
